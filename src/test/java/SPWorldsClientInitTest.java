be1cf2cac60c19cb869101ae52473ad44b36f383
R4mBLeR <99557962+R4mBLeR@users.noreply.github.com>
2025-08-19 01:49:27 +0300

    Delete src/test directory

--- src/test/java/SPWorldsClientInitTest.java
+++
@@ -1,9 +0,0 @@
-import net.r4mble.SPWorldsClient;
-import org.junit.jupiter.api.Test;
-
-public class SPWorldsClientInitTest {
-    @Test
-    void testAPI() {
-        SPWorldsClient SPWorldsClient = new SPWorldsClient("59af8cac-052b-4eef-8a88-fb3827565aa4", "EvZQcI+xHEPA2N4J63da3UBgOcvCLPEP");
-    }
-}
